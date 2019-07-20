d723f31dfab94202110c02aa0da68b88053a5003
shimura plumsa <shimura@plumsa.co.jp>
2019-07-20 15:41:38 +0900

    TaskSeeder追加

---
+++ database/seeds/TaskSeeder.php
@@ -0,0 +1,37 @@
+<?php
+
+use App\Task;
+use Illuminate\Database\Seeder;
+
+class TaskSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $this->initTasks();
+    }
+
+    public function initTasks()
+    {
+        $tasks = [
+            ['タスク１', 1],
+            ['タスク２', 1],
+            ['タスク３', 1],
+            ['タスク４', 1],
+            ['タスク５', 1],
+        ];
+
+        Task::truncate();
+
+        foreach ($tasks as $task) {
+            Task::create([
+                'contents'              => $task[0],
+                'users_id'           => $task[1],
+            ]);
+        }
+    }
+}
