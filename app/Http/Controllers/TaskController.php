19960a08965cf4b025a0850a9cef54cad155342d
shimura plumsa <shimura@plumsa.co.jp>
2019-07-20 16:45:20 +0900

    TaskControllerにaddTaskを追加

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -2,8 +2,9 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
 use App\Task;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class TaskController extends Controller
 {
@@ -34,5 +35,28 @@ class TaskController extends Controller
 
         return $tasks;
     }
-}
 
+    public function addTask(Request $request)
+    {
+        $data = $request->all();
+
+        if (!$user = Auth::user()) {
+            $user_id = 1;
+        } else {
+            $user_id = $user->id;
+        }
+
+        // システムデータ
+        $system_data = [
+            'users_id' => $user_id
+        ];
+
+        $result = Task::create(array_merge($data, $system_data));
+
+        return Response::json([
+            $result->toArray()
+        ], 201);
+    }
+
+
+}
